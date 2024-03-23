d946ab4fe0c3e912f85f782e6e4a3fb0426fd2a7
apk10 <arun.sv101@gmail.com>
2024-03-24 02:04:34 +0530

    Added output training on webhook

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
 	System.out.println("Welcome to Jenkins Training");
+	 System.out.println("Testing Webhook");
     }
 }
